80e719a52b696475aebb8e99705213c80db140a5
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-08-20 13:34:56 -0600

    added features kick, punch

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('hello')
+// data
+let health = 100
+
+//program
+
+function slap(){
+  health -=1
+  console.log(health)
+update()
+}
+function punch(){
+  health -=5
+  console.log(health)
+update()
+}
+function kick(){
+  health -=10
+  console.log(health)
+update()
+}
+//Draw
+function update(){
+  document.getElementById("health").innerText = health.toString()
+}
+
+
+update()
